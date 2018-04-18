d31c4168cf5910634ea94a763a3808c7efe3cd31
Manuel Guillen <manuelg@mit.edu>
2018-04-18 15:26:38 -0400

    Added audio retention between scenes.

---
+++ 6.073 Project 4/Assets/Scripts/AudioRetainerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRetainerScript : MonoBehaviour {
+
+	private static AudioRetainerScript instance = null;
+
+	public static AudioRetainerScript Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
