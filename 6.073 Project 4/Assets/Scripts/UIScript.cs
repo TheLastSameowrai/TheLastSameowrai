19f9fa6703598393a017742b2f2f21a6aabc93cb
TLiang518 <terranceliang01@gmail.com>
2018-04-18 15:36:59 -0400

    Timer UI Added

---
+++ 6.073 Project 4/Assets/Scripts/UIScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+
+    public Text Timer;
+    float start_time;
+
+    // Use this for initialization
+    void Start () {
+        start_time = Time.time;
+        Timer.text = "00:00";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float elapsedTime = Time.time - start_time;
+        int minutes = ((int)elapsedTime) / 60;
+        Timer.text = minutes.ToString("D2") + ":" + ((int)(elapsedTime) - 60 * minutes).ToString("D2");
+    }
+}
