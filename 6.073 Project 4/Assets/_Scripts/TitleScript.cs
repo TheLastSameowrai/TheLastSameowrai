463a531c3aab0411a4a6b660508833c3383a57de
Marco Cunqueiro <cunqueim@mit.edu>
2018-04-18 11:28:03 -0400

    Adding in old title and instructions scenes

---
+++ 6.073 Project 4/Assets/_Scripts/TitleScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution (1500, 900, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ToGameScene() {
+		SceneManager.LoadScene ("GameScene");
+	}
+
+	public void ToInstructionsScene() {
+		SceneManager.LoadScene ("InstructionsScene");
+	}
+
+}
