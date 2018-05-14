6d4472565f50193b998d2091e02ae023c4d225ce
Manuel Guillen <manuelg@mit.edu>
2018-05-14 15:41:03 -0400

    Parry sound works without breaking attack sounds.

--- 6.073 Project 4/Assets/_Scripts/PlayerController.cs
+++ 6.073 Project 4/Assets/_Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour {
 
     private AudioSource aud;
 
-    public AudioClip lowSound, midSound, highSound;
+    public AudioClip lowSound, midSound, highSound, blockSound;
 
     // Use this for initialization
     void Start () {
@@ -37,25 +37,27 @@ public class PlayerController : MonoBehaviour {
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 ph.RequestParry();
+                aud.clip = blockSound;
                 parryFlag = true;
             }
 			if (Input.GetKeyDown (KeyCode.Space)) {
-				ah.RequestAttack ();
+				ah.RequestAttack();
+                switch(sm.currentStance) {
+                    case StanceManager.Stance.LOW:  aud.clip = lowSound;    break;
+                    case StanceManager.Stance.MID:  aud.clip = midSound;    break;
+                    case StanceManager.Stance.HIGH: aud.clip = highSound;   break;
+                }
 				attackFlag = true;
 			}
 			if (Input.GetKeyDown(KeyCode.W))
 			{
 				sm.StanceUp();
-                if (sm.currentStance == StanceManager.Stance.HIGH) aud.clip = highSound;
-                else aud.clip = midSound;
 			} else if (Input.GetKeyDown(KeyCode.S))
 			{
-				sm.StanceDown ();
-                if (sm.currentStance == StanceManager.Stance.LOW) aud.clip = lowSound;
-                else aud.clip = midSound;
+                sm.StanceDown();
             }
 
-			if (attackFlag) aud.Play();
+			if (attackFlag || parryFlag) aud.Play();
 		}
 
 		if (transform.position.x - Camera.main.transform.position.x > 2) {
